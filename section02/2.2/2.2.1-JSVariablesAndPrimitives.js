8b1e606105933639d36c93707f1b8ce8d09f71b7
Vann Calhoune <vanncalhoune@Vanns-Air.lan>
2022-08-29 18:01:06 -0500

    Class Activities

---
+++ section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,22 @@
+// create a constant variable (const)
+const myName = "Vann";
+// create a variable that can be reassigned (let)
+let age = 26;
+// create three variables and assign them values of different data types (=)
+let skill = "Dunk";
+let tired = false;
+let points = 81;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof "Dunk");
+console.log(typeof 81);
+// change the value referenced by a variable (dynamic typing)
+age = 46;
+// print the type of the variable you just changed (typeof)
+console.log(typeof 46);
+// create variables and give them values to complete the sentences that will print  below.
+let language = "English";
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+console.log("Hello, my name is " + myName + ", and I am learning " + language + ". I have been practicing for only " +  1854 + " weeks, so my status as a master is: " + true + ".");
---
+++ section02/2.3/2.3.2-ComparisonOperatorsAndIfElseStatements.js
@@ -0,0 +1,16 @@
+let age = 25;
+
+if (age >= 25) {
+  console.log("Renting a car is more affordable.");
+}
+
+if (age < 25) {
+  console.log("Renting a car is very expensive.");
+}
+if (age >= 25) {
+  console.log("Renting a car is more affordable.");
+} else if (age >= 18) {
+  console.log("Renting a car is very expensive.");
+} else {
+  console.log("You cannot legally rent a car.");
+}
